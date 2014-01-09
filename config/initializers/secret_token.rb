2ae20f3810586a49a8e819645d70219240162158
Liu <yingjil@uc81f6609558852809e94.ant.amazon.com>
2014-01-09 03:38:25 +0000

    new BulkConsole

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BulkConsole::Application.config.secret_key_base = 'c2a195b1b2e21690ef4d04fba5900e46e4ecf30bb39a104f4b68602af91e04bbc1d29573839b6944a85867bfabaa27c6f659fbb7ab85aa0f1b078f4b48b4811a'
